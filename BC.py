75e0824c368b65c03e37f088385b3942c30f8897
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-18 12:37:04 +0200

    Update BC.py

--- BC.py
+++ BC.py
@@ -1,1 +1,13 @@
-
+def baseToDec(number : str, base : int):
+    number, result = number.split(","), 0
+    for value in range(len(number)-1): result += int(number[len(number)-value]) * (base**value)
+    return result
+    
+def decToBase(number : int, base : int):
+    result = ""
+    while number >= base: number ,result = number // base,f'{number % base},' + result
+    return result[:-1]
+    
+def main():
+    iN, iB, oB = input("Input number:\n"), int(input("Input base:\n")), int(input("Output base:\n"))
+    print(decToBase(baseToDec(iN, iB), oN)
